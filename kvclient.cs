648bcb857297a3278bcde2f9614cb679522b844e
Munieshwar Ramdass <mramdass@nyu.edu>
2016-12-01 22:50:09 -0500

    Update kvclient.cs

--- kvclient.cs
+++ kvclient.cs
@@ -24,20 +24,25 @@ namespace kvclient {
                     int.TryParse(str[1], out port);
                 }
             }
-            
-            var transport = new TSocket("localhost", 9090);
-            var protocol = new TBinaryProtocol(transport);
-            var client = new KVStore.Client(protocol);
-            
-            Result result = new Result();
-            for (int i = 0; i < args.Length; ++i) {
-                if (args[i] == "-set") result = client.kvset(args[i + 1], args[i + 2]);
-                else if (args[i] == "-get") result = client.kvget(args[i + 1]);
-                else if (args[i] == "-delete") result = client.kvdelete(args[i + 1]);
-            }
-            Console.WriteLine("\tValue: " + result.Value + "\n\tErrorCode: " + result.Error + "\n\tErrorText" + result.Errortext);
 
-            transport.Open();
+            try
+            {
+                var transport = new TSocket("localhost", 9090);
+                var protocol = new TBinaryProtocol(transport);
+                var client = new KVStore.Client(protocol);
+
+                Result result = new Result();
+                for (int i = 0; i < args.Length; ++i)
+                {
+                    if (args[i] == "-set") result = client.kvset(args[i + 1], args[i + 2]);
+                    else if (args[i] == "-get") result = client.kvget(args[i + 1]);
+                    else if (args[i] == "-delete") result = client.kvdelete(args[i + 1]);
+                }
+                Console.WriteLine("\tValue: " + result.Value + "\n\tErrorCode: " + result.Error + "\n\tErrorText" + result.Errortext);
+
+                transport.Open();
+            }
+            catch (Exception e) { Console.WriteLine(e.ToString()); }
         }
     }
 }
